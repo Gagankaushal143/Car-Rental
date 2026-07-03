7c77c2cd63192fc93cf12555591bda6f9239ee1c
user.email <gagankaushal143@gmail.com>
2026-07-03 23:28:17 +0530

    added authentication

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -15,7 +15,7 @@ export const registerUser = async (req, res) => {
         }
 
         const existingUser = await User.findOne({ email });
-        
+
         if (existingUser) {
             return res.status(409).json({
                 success: false,
@@ -30,50 +30,161 @@ export const registerUser = async (req, res) => {
             firstName,
             lastName,
             email,
-            password : hashedPassword,
+            password: hashedPassword,
             phone,
         })
 
         const token = jwt.sign(
             {
-                userId : user._id,
-                role : user.role,
+                userId: user._id,
+                role: user.role,
             },
             process.env.JWT_SECRET,
             {
-                expiresIn : "7d"
+                expiresIn: "7d"
             }
         )
-        
+
         return res.status(201).json({
-            success : true,
-            message : "User registered successfully ",
+            success: true,
+            message: "User registered successfully ",
             token,
             data: {
-                id : user._id,
-                firstName : user.firstName,
-                email : user.email,
+                id: user._id,
+                firstName: user.firstName,
+                email: user.email,
             },
         })
 
     }
-    
-    catch(error){
+
+    catch (error) {
         return res.status(500).json({
             success: false,
-            message : "Internal Server Error",
+            message: "Internal Server Error",
         });
     }
 
 };
 
-export const getCurrentUser = (req, res) =>{
+export const getCurrentUser = async (req, res) => {
+    try {
+        const user = await User.findById(req.user.userId).select("-password");
 
-    
-    return res.status(200).json({
-        success : true,
-        user : req.user,
-        // message : "Protected route accessed",
-    })
-    console.log(req.user);
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                message: "User not found",
+            })
+        }
+        return res.status(200).json({
+            success: true,
+            user,
+        })
+    }
+    catch(error){
+        return res.status(500).json({
+            success : false,
+            message : "Internal Server Error"
+        })
+    }
+}
+
+
+export const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!email?.trim() || !password?.trim()) {
+            return res.status(400).json({
+                success: false,
+                message: "Email and Password are required !",
+            });
+        }
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(401).json({
+                success: false,
+                message: "Invalid email or password !",
+            });
+        }
+
+        const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordMatch) {
+            return res.status(401).json({
+                success: false,
+                message: "Invalid email or password !"
+            })
+        }
+
+        const token = jwt.sign(
+            {
+                userId: user._id,
+                role: user.role,
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "7d",
+            }
+        )
+
+        return res.status(200).json({
+            success: true,
+            message: "Login Successful",
+            token,
+            data: {
+                userId: user._id,
+                firstName: user.firstName,
+                email: user.email,
+            }
+        })
+    }
+    catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server Error"
+        });
+    }
+}
+
+
+export const updateProfile = async (req, res) =>{
+    try{
+        const {firstName, lastName, phone, avatar} = req.body;
+        if(!firstName?.trim() && !lastName?.trim() && !phone?.trim()){
+            return res.status(400).json({
+                success : false,
+                message : "At least one field is required to update !"
+            })
+        }
+
+        const updateData = {};
+
+        if(firstName?.trim()) updateData.firstName = firstName.trim();
+        if(lastName?.trim()) updateData.lastName = lastName.trim();
+        if(phone?.trim()) updateData.phone = phone.trim();
+        if(avatar) updateData.avatar = avatar;
+
+        const user = await User.findByIdAndUpdate(
+            req.user.userId,
+            updateData,
+            {
+                new : true,
+            }
+        ).select("-password")
+
+        return res.status(200).json({
+            success : true, 
+            message : "Profile updated successfully !",
+            data : user
+        })
+    }
+    catch(error){
+        return res.status(500).json({
+            success : false,
+            message : "Internal Server Error",
+        });
+    }
 }
--- Backend/routes/auth.routes.js
+++ Backend/routes/auth.routes.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { registerUser } from "../controllers/auth.controller.js";
-import {getCurrentUser} from "../controllers/auth.controller.js"
+import {registerUser, getCurrentUser, loginUser, updateProfile} from "../controllers/auth.controller.js"
 import { protect } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
+router.post("/login", loginUser)
 router.get("/me", protect, getCurrentUser);
+router.put("/profile", protect, updateProfile)
 
 export default router;
