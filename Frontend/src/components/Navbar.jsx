83c43f709623a22b1c2bdf5ab4350d7cd950e9fc
user.email <gagankaushal143@gmail.com>
2026-07-11 23:43:56 +0530

    added some changes

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,13 +1,13 @@
-import { Link } from "react-router-dom"
+import { Link , NavLink } from "react-router-dom"
 
 export const Navbar = () => {
   return (
-    <nav className="flex items-center justify-between px-6 h-16 max-w-7xl rounded-2xl mx-auto bg-gray-900 text-white mt-1 sticky">
+    <nav className="flex items-center justify-between px-6 h-18 max-w-7xl rounded-3xl mx-auto bg-gray-900 text-white mt-1 sticky">
       <div>
         <Link to="/" className="text-orange-500 font-bold text-2xl">🚗 RentalX</Link>
       </div>
-      <div className="space-x-6">
-        <Link to="/" className="hover:text-orange-500 transition-colors duration-300">Home</Link>
+      <div className="space-x-8">
+        <NavLink to="/" className={({isActive}) => `transition-colors duration-300 ${isActive ? "text-orange-500" : "text-white hover:text-orange-500"}`}>Home</NavLink>
         <Link to="/cars" className="hover:text-orange-500 transition-colors duration-300">Cars</Link>
         <Link to="/about" className="hover:text-orange-500 transition-colors duration-300">About</Link>
       </div>
