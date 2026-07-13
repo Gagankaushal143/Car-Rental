2d7e95e1f5c2209a7febcd40662c1582502434ce
user.email <gagankaushal143@gmail.com>
2026-07-14 00:58:23 +0530

    Added Hero section

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -2,14 +2,14 @@ import { Link , NavLink } from "react-router-dom"
 
 export const Navbar = () => {
   return (
-    <nav className="flex items-center justify-between px-6 h-18 max-w-7xl rounded-3xl mx-auto bg-gray-900 text-white mt-1 sticky">
+    <nav className="flex items-center justify-between px-6 h-18 max-w-7xl rounded-3xl mx-auto bg-gray-900 text-white sticky top-0">
       <div>
         <Link to="/" className="text-orange-500 font-bold text-2xl">🚗 RentalX</Link>
       </div>
       <div className="space-x-8">
-        <NavLink to="/" className={({isActive}) => `transition-colors duration-300 ${isActive ? "text-orange-500" : "text-white hover:text-orange-500"}`}>Home</NavLink>
-        <Link to="/cars" className="hover:text-orange-500 transition-colors duration-300">Cars</Link>
-        <Link to="/about" className="hover:text-orange-500 transition-colors duration-300">About</Link>
+        <NavLink to="/" className={({isActive}) => `transition-colors duration-300 ${isActive ? "text-orange-400" : "text-white hover:text-orange-500"}`}>Home</NavLink>
+        <NavLink to="/cars" className={({isActive}) => `transition-colors duration-300 ${isActive ? "text-orange-400" : "text-white hover:text-orange-500"}`}>Cars</NavLink>
+        <NavLink to="/about" className={({isActive}) => `transition-colors duration-300 ${isActive ? "text-orange-400" : "text-white hover:text-orange-500"}`}>About</NavLink>
       </div>
 
       <div className="space-x-4">
